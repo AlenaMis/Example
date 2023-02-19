7fba8b12636d300f9f57344146f3de8529ce2568
Alena <alenamis90@mail.ru>
2023-02-20 00:57:04 +0300

    Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+﻿int numberA = new Random().Next(1,20); // 1 2 3 4 ... 19
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10);
+int numberB = new Random().Next(1,20);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
